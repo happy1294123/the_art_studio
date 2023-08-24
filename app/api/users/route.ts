f94daca2d232a07bc98e28c838244d1e415e0a30
Allen_Yeh <happy1294123@gmail.com>
2023-08-24 19:39:23 +0800

    test drizzle 2

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from 'next/server'
-import getAllDbData from '@/lib/getAllDbData'
+// import { NextResponse } from 'next/server'
+// import getAllDbData from '@/lib/getAllDbData'
 
-export async function GET(request: Request) {
-  const users = await getAllDbData()
-  return NextResponse.json(users)
-}
+// export async function GET(request: Request) {
+//   const users = await getAllDbData()
+//   return NextResponse.json(users)
+// }
