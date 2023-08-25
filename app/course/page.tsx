b627a5afea7737e829d8445806052fa50b124d04
Allen_Yeh <happy1294123@gmail.com>
2023-08-25 15:58:46 +0800

    deploy2

--- app/course/page.tsx
+++ app/course/page.tsx
@@ -2,7 +2,7 @@ import CoursesShower from '@/components/course/CoursesShower'
 import TheTitle from '@/components/TheTitle'
 
 async function getCourses(): Promise<Course[]> {
-  const res = await fetch(`${process.env.HOST}/api/course`, { cache: 'no-cache' })
+  const res = await fetch(`${process.env.HOST}/api/course`)
   return res.json()
 }
 
