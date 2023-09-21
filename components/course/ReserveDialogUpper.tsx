71e76b994d48ddc13c5af505418f58cb7a97565a
Allen_Yeh <happy1294123@gmail.com>
2023-09-21 16:34:08 +0800

    test: open graph line5

--- components/course/ReserveDialogUpper.tsx
+++ components/course/ReserveDialogUpper.tsx
@@ -67,7 +67,8 @@ export default function ReserveDialogUpper({ course, setOpen, mutateReservation,
                     data-[state=closed]:animate-[dialog-content-hide_300ms] min-w-[10px]">
             <DropdownMenuLabel >
               <div>
-                <a href={`https://social-plugins.line.me/lineit/share?url=https://the-art-studio.vercel.app?id=${course.id}&date=${dateFormatter(new Date(course.date))}`}
+                <a href={`https://social-plugins.line.me/lineit/share?url=https://the-art-studio.vercel.app/course?id=${course.id}&date=${dateFormatter(new Date(course.date))}`}
+                  target='_blank'
                   className='flex gap-1'>
                   <FaLine size={21} />分享Line
                 </a>
