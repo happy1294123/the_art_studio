73ed0f2941ac47714703240bcc84c2e919fb963d
Allen_Yeh <happy1294123@gmail.com>
2023-11-26 19:47:58 +0800

    feat: add cron script

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -3,68 +3,65 @@ import prisma from "@/lib/initPrisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  console.log('test cron');
+  const now = new Date()
+  const tomorrow = now.setDate(now.getDate() + 1)
 
-  return Response.json(new Date());
-  // const now = new Date()
-  // const tomorrow = now.setDate(now.getDate() + 1)
+  const tomorrowString = dateFormatter(new Date(tomorrow), '/', true)
 
-  // const tomorrowString = dateFormatter(new Date(tomorrow), '/', true)
+  const tomorrowCourses = await prisma.course.findMany({
+    where: {
+      date: tomorrowString
+    },
+    select: {
+      id: true,
+      date: true,
+      start_time: true,
+      end_time: true,
+      baseline_rez: true,
+      isOpen: true,
+    }
+  })
 
-  // const tomorrowCourses = await prisma.course.findMany({
-  //   where: {
-  //     date: tomorrowString
-  //   },
-  //   select: {
-  //     id: true,
-  //     date: true,
-  //     start_time: true,
-  //     end_time: true,
-  //     baseline_rez: true,
-  //     isOpen: true,
-  //   }
-  // })
+  const checkCourse = tomorrowCourses.filter(course => {
+    const start = new Date(`${course.date} ${course.start_time}`).getTime()
+    const end = new Date(`${course.date} ${course.end_time}`).getTime()
+    return start < tomorrow && tomorrow < end
+  })
 
-  // const checkCourse = tomorrowCourses.filter(course => {
-  //   const start = new Date(`${course.date} ${course.start_time}`).getTime()
-  //   const end = new Date(`${course.date} ${course.end_time}`).getTime()
-  //   return start < tomorrow && tomorrow < end
-  // })
+  if (checkCourse.length > 1) {
+    console.log('發生錯誤，同時段有兩個課程');
+    return NextResponse.json('發生錯誤，同時段有兩個課程', { status: 400 })
+  }
 
-  // if (checkCourse.length > 1) {
-  //   console.log('發生錯誤，同時段有兩個課程');
-  //   return NextResponse.json('發生錯誤，同時段有兩個課程', { status: 400 })
-  // }
+  if (checkCourse.length === 0) {
+    console.log('24小時後無課程');
+    return NextResponse.json('24小時後無課程', { status: 400 })
+  }
 
-  // if (checkCourse.length === 0) {
-  //   console.log('24小時後無課程');
-  //   return NextResponse.json('24小時後無課程', { status: 400 })
-  // }
+  // to be confirmed course
+  const tbcCourse = checkCourse[0]
+  const reservationNum = await prisma.reservation.count({
+    where: {
+      course_id: tbcCourse.id
+    }
+  })
 
-  // // to be confirmed course
-  // const tbcCourse = checkCourse[0]
-  // const reservationNum = await prisma.reservation.count({
-  //   where: {
-  //     course_id: tbcCourse.id
-  //   }
-  // })
+  let isOpen: boolean
+  if (reservationNum < tbcCourse.baseline_rez) { // 確定開課
+    isOpen = false
+  } else {
+    // make notify
+    isOpen = true
+  }
 
-  // let isOpen: boolean
-  // if (reservationNum < tbcCourse.baseline_rez) { // 確定開課
-  //   isOpen = false
-  // } else {
-  //   // make notify
-  //   isOpen = true
-  // }
+  await prisma.course.update({
+    where: {
+      id: tbcCourse.id
+    },
+    data: {
+      isOpen
+    }
+  })
 
-  // await prisma.course.update({
-  //   where: {
-  //     id: tbcCourse.id
-  //   },
-  //   data: {
-  //     isOpen
-  //   }
-  // })
-
-  // return NextResponse.json(checkCourse)
+  return NextResponse.json(checkCourse)
 }
--- components/home/HomePageOnly.tsx
+++ components/home/HomePageOnly.tsx
@@ -1,14 +1,28 @@
 'use client'
 import dynamic from 'next/dynamic'
 import { usePathname } from 'next/navigation'
+import { useEffect, useState } from 'react'
 const GoogleArea = dynamic(() => import('@/components/home/GoogleArea'), { ssr: false })
 
 export default function HomePageOnly({ apiKey }: { apiKey: string | undefined }) {
+  const [show, setShow] = useState(false)
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 2000) {
+        setShow(true)
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
+
   const pathname = usePathname()
 
   return (
     <div>
-      {(pathname === '/' && apiKey) && <GoogleArea apiKey={apiKey} />}
+      {(pathname === '/' && apiKey && show) && <GoogleArea apiKey={apiKey} />}
     </div>
   )
 }
