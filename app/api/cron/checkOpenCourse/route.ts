833558b40402d7844d054e3c58df32696541759a
Allen_Yeh <happy1294123@gmail.com>
2023-11-26 17:23:23 +0800

    fix: cron14

--- app/api/cron/checkOpenCourse/route.ts
+++ app/api/cron/checkOpenCourse/route.ts
@@ -5,65 +5,74 @@ import { NextResponse } from "next/server";
 export const dynamic = 'force-dynamic'
 
 export async function GET() {
-  const now = new Date()
-  const tomorrow = now.setDate(now.getDate() + 1)
-
-  const tomorrowString = dateFormatter(new Date(tomorrow), '/', true)
-
-  const tomorrowCourses = await prisma.course.findMany({
-    where: {
-      date: tomorrowString
+  const result = await fetch(
+    'http://worldtimeapi.org/api/timezone/America/Chicago',
+    {
+      cache: 'no-store',
     },
-    select: {
-      id: true,
-      date: true,
-      start_time: true,
-      end_time: true,
-      baseline_rez: true,
-      isOpen: true,
-    }
-  })
+  );
+  const data = await result.json();
 
-  const checkCourse = tomorrowCourses.filter(course => {
-    const start = new Date(`${course.date} ${course.start_time}`).getTime()
-    const end = new Date(`${course.date} ${course.end_time}`).getTime()
-    return start < tomorrow && tomorrow < end
-  })
+  return Response.json({ datetime: data.datetime });
+  // const now = new Date()
+  // const tomorrow = now.setDate(now.getDate() + 1)
 
-  if (checkCourse.length > 1) {
-    console.log('發生錯誤，同時段有兩個課程');
-    return NextResponse.json('發生錯誤，同時段有兩個課程', { status: 400 })
-  }
+  // const tomorrowString = dateFormatter(new Date(tomorrow), '/', true)
 
-  if (checkCourse.length === 0) {
-    console.log('24小時後無課程');
-    return NextResponse.json('24小時後無課程', { status: 400 })
-  }
+  // const tomorrowCourses = await prisma.course.findMany({
+  //   where: {
+  //     date: tomorrowString
+  //   },
+  //   select: {
+  //     id: true,
+  //     date: true,
+  //     start_time: true,
+  //     end_time: true,
+  //     baseline_rez: true,
+  //     isOpen: true,
+  //   }
+  // })
 
-  // to be confirmed course
-  const tbcCourse = checkCourse[0]
-  const reservationNum = await prisma.reservation.count({
-    where: {
-      course_id: tbcCourse.id
-    }
-  })
+  // const checkCourse = tomorrowCourses.filter(course => {
+  //   const start = new Date(`${course.date} ${course.start_time}`).getTime()
+  //   const end = new Date(`${course.date} ${course.end_time}`).getTime()
+  //   return start < tomorrow && tomorrow < end
+  // })
 
-  let isOpen: boolean
-  if (reservationNum < tbcCourse.baseline_rez) { // 確定開課
-    isOpen = false
-  } else {
-    // make notify
-    isOpen = true
-  }
+  // if (checkCourse.length > 1) {
+  //   console.log('發生錯誤，同時段有兩個課程');
+  //   return NextResponse.json('發生錯誤，同時段有兩個課程', { status: 400 })
+  // }
 
-  await prisma.course.update({
-    where: {
-      id: tbcCourse.id
-    },
-    data: {
-      isOpen
-    }
-  })
+  // if (checkCourse.length === 0) {
+  //   console.log('24小時後無課程');
+  //   return NextResponse.json('24小時後無課程', { status: 400 })
+  // }
 
-  return NextResponse.json(checkCourse)
+  // // to be confirmed course
+  // const tbcCourse = checkCourse[0]
+  // const reservationNum = await prisma.reservation.count({
+  //   where: {
+  //     course_id: tbcCourse.id
+  //   }
+  // })
+
+  // let isOpen: boolean
+  // if (reservationNum < tbcCourse.baseline_rez) { // 確定開課
+  //   isOpen = false
+  // } else {
+  //   // make notify
+  //   isOpen = true
+  // }
+
+  // await prisma.course.update({
+  //   where: {
+  //     id: tbcCourse.id
+  //   },
+  //   data: {
+  //     isOpen
+  //   }
+  // })
+
+  // return NextResponse.json(checkCourse)
 }
